2e406b6ddbe209635678a8c9a0a74b3b74600acf
tirthajyoti-ghosh <itirthahere@gmail.com>
2021-09-06 18:09:43 +0530

    Add helpful comments

--- collapsible.js
+++ collapsible.js
@@ -2,20 +2,21 @@ const openIcon = `<svg width="15" height="15" role="img" xmlns="http://www.w3.or
 
 const collapseIcon = `<svg width="15" height="15" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#000" d="M34.5 239L228.9 44.7c9.4-9.4 24.6-9.4 33.9 0l22.7 22.7c9.4 9.4 9.4 24.5 0 33.9L131.5 256l154 154.7c9.3 9.4 9.3 24.5 0 33.9l-22.7 22.7c-9.4 9.4-24.6 9.4-33.9 0L34.5 273c-9.3-9.4-9.3-24.6 0-34zm192 34l194.3 194.3c9.4 9.4 24.6 9.4 33.9 0l22.7-22.7c9.4-9.4 9.4-24.5 0-33.9L323.5 256l154-154.7c9.3-9.4 9.3-24.5 0-33.9l-22.7-22.7c-9.4-9.4-24.6-9.4-33.9 0L226.5 239c-9.3 9.4-9.3 24.6 0 34z"></path></svg>`;
 
+// Inject a script tag that contains click event handler code that makes toggling work
 const newScript = document.createElement("script");
 const inlineScript = document.createTextNode(`
     const toggleNavbar = (action) => {
-        const nav = document.getElementById('left-sidebar');
-        const sideNavToggler = document.getElementById('sidenav-toggler');
+        const nav = document.getElementById('left-sidebar'); // nav tag
+        const sideNavToggler = document.getElementById('sidenav-toggler'); // toggler icon div tag
 
         if (action === 'collapse') {
-            sideNavToggler.innerHTML = '${openIcon}';
-            sideNavToggler.setAttribute('onclick', "toggleNavbar('open')");
+            sideNavToggler.innerHTML = '${openIcon}'; // If action is 'collapse' then change the icon to 'open' icon
+            sideNavToggler.setAttribute('onclick', "toggleNavbar('open')"); // Update the argument of event handler function to 'open'
             
             nav.style.width = 0;
         } else {
-            sideNavToggler.innerHTML = '${collapseIcon}';
-            sideNavToggler.setAttribute('onclick', "toggleNavbar('collapse')");
+            sideNavToggler.innerHTML = '${collapseIcon}'; // If action is 'open' then change the icon to 'collapse' icon
+            sideNavToggler.setAttribute('onclick', "toggleNavbar('collapse')"); // Update the argument of event handler function to 'collapse'
 
             nav.style.width = '300px';
         }
@@ -25,16 +26,19 @@ newScript.appendChild(inlineScript);
 const head = document.getElementsByTagName('head')[0];
 head.appendChild(newScript);
 
+// Set styles to main tag
 const main = document.querySelector('#app > div.row.flex-nowrap > main.col');
 main.style.position = 'relative';
 main.style.zIndex = '99';
 main.style.transition = 'all 0.5s';
 
+// Set styles to nav tag
 const nav = document.getElementById('left-sidebar');
 nav.style.transition = 'all 0.5s';
 nav.style.overflowX = 'hidden';
 nav.style.whiteSpace = 'nowrap';
 
+// Prepend the toggler icon to header tag
 const header = document.querySelector('#app > div.row.flex-nowrap > main.col > header.row.top-navbar-section.p-3');
 const headerInnerHTML = header.innerHTML;
 header.innerHTML = `<div onclick="toggleNavbar('collapse')" id="sidenav-toggler" style="position: absolute; left: 0; top: 22px; z-index: 999; padding: 5px; border: 1.5px solid #000; border-left: 0; border-radius: 0 3px 3px 0; cursor: pointer">${collapseIcon}</div>${headerInnerHTML}`;
